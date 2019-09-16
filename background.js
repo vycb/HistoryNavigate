e3eda87a37865d7597baef25759832d0bad58db9
vycb <vycb777@gmail.com>
2019-09-16 19:15:22 +0300

    v2.0.0

--- background.js
+++ background.js
@@ -7,6 +7,7 @@ function setUpMenu(){
 		chrome.contextMenus.removeAll()
 		let i=0
 		for (let item of historyItems) {
+			let url = new URL(item.url)
 			chrome.contextMenus.create({ id: "historynavigate-" + (i++), title: item.title, contexts: ["all"], enabled: true, onclick: ()=>{
 				let querying = browser.tabs.query({currentWindow: true, active: true})
 				querying.then((tabs)=>{
@@ -16,7 +17,8 @@ function setUpMenu(){
 					})
 					//console.log(`url: ${item.url}`)
 				}, (error)=>{	console.log(`Error: ${error}`) })
-			} })
+			}
+			, icons: {16: url.origin+"/favicon.ico"} })
 		}
 	}
 	searching.then(listVisits)
