9985487aba830d2e82887e4f5b117b6561354f27
vycb <vycb777@gmail.com>
2019-09-17 20:29:25 +0300

    v2.1.0

--- background.js
+++ background.js
@@ -3,38 +3,42 @@ function setUpMenu(){
 		text: "",
 		maxResults: 50
 	})
+
 	function listVisits(historyItems){
 		chrome.contextMenus.removeAll()
 		let i=0
 		for (let item of historyItems) {
-			let url = new URL(item.url)
-			chrome.contextMenus.create({ id: "historynavigate-" + (i++), title: item.title, contexts: ["all"], enabled: true, onclick: ()=>{
+			let url = new URL(item.url),
+				itemTitle = item.title? item.title: item.url
+	
+			chrome.contextMenus.create({ id: "historynavigate-" + i++, title: itemTitle, contexts: ["all"], enabled: true, onclick: ()=>{
 				let querying = browser.tabs.query({currentWindow: true, active: true})
-				querying.then((tabs)=>{
+				querying.then(tabs=>{
 					browser.tabs.update(tabs[0].id, {
 						active: true,
 						url: item.url
 					})
 					//console.log(`url: ${item.url}`)
-				}, (error)=>{	console.log(`Error: ${error}`) })
+				}, error=>{	console.log(`Error: ${error}`) })
 			}
 			, icons: {16: url.origin+"/favicon.ico"} })
 		}
 	}
+
 	searching.then(listVisits)
 
 }
 
-chrome.webNavigation.onCompleted.addListener(  /* page loaded */
+chrome.webNavigation.onCompleted.addListener( /* page loaded */
 	setUpMenu
 )
 
-chrome.tabs.onUpdated.addListener(  /* URL updated */
+chrome.tabs.onUpdated.addListener( /* URL updated */
 	setUpMenu
 )
 
 
-chrome.tabs.onActivated.addListener(  /* tab selected */
+chrome.tabs.onActivated.addListener( /* tab selected */
 	setUpMenu
 )
 
