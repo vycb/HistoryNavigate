d789c4606f2553f2d56a26748822909cbdb4a553
vycb <vycb777@gmail.com>
2019-10-09 16:11:25 +0300

    v2.2.2

--- background.js
+++ background.js
@@ -1,50 +1,58 @@
 function setUpMenu(){
-	var searching = browser.history.search({
+	let today = new Date(),
+			pastYear = today.getFullYear() - 1,
+			sdate = new Date(pastYear+"-01-01"),
+			searching = browser.history.search({
 		text: "",
-		maxResults: 50
+		startTime: sdate.toISOString()
+		,maxResults: 125
 	})
 
 	function listVisits(historyItems){
-		chrome.contextMenus.removeAll()
+		browser.menus.removeAll() //{{{
 		let i=0
 		for (let item of historyItems) {
 			let url = new URL(item.url),
 				itemTitle = item.title? item.title: item.url;
 
 			if(itemTitle.length >71 ){
-				let beg = itemTitle.slice(0, 32)
-				let end = itemTitle.slice(-35)
-				itemTitle = beg+'...'+end
+				try{
+					const beg = itemTitle.slice(0, 32)
+					const end = itemTitle.slice(-35)
+					itemTitle = beg+'...'+end
+				}catch(err){
+					console.log(`itemTitle: ${error}`)
+				}
 			}
 	
-			chrome.contextMenus.create({ id: "historynavigate-" + i++, title: itemTitle, contexts: ["all"], enabled: true, onclick: ()=>{
-				let querying = browser.tabs.query({currentWindow: true, active: true})
+			browser.menus.create({ id: "historynavigate-" + i++, title: itemTitle, contexts: ["all"], enabled: true, onclick: (info, tab)=>{
+				const querying = browser.tabs.query({currentWindow: true, active: true})
 				querying.then(tabs=>{
 					browser.tabs.update(tabs[0].id, {
 						active: true,
 						url: item.url
 					})
 					//console.log(`url: ${item.url}`)
-				}, error=>{	console.log(`Error: ${error}`) })
+				}, error=>{	console.log(`querying: ${error}`) })
 			}
 			, icons: {16: url.origin+"/favicon.ico"} })
 		}
-	}
+	} //}}}
 
 	searching.then(listVisits)
 
 }
 
-chrome.webNavigation.onCompleted.addListener( /* page loaded */
+browser.webNavigation.onCompleted.addListener( /* page loaded */
 	setUpMenu
 )
 
-chrome.tabs.onUpdated.addListener( /* URL updated */
+browser.tabs.onUpdated.addListener( /* URL updated */
 	setUpMenu
 )
 
 
-chrome.tabs.onActivated.addListener( /* tab selected */
+browser.tabs.onActivated.addListener( /* tab selected */
 	setUpMenu
 )
 
